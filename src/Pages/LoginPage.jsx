0394ff77d99f83d162e7517d3e0b4f196ac26c09
AtharvKhamkar <khamkaratharv2002@gmail.com>
2024-07-10 20:40:04 +0530

    redesigned css

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import '../CSS/LoginPage.css';
+import { apple_logo, google_logo, meta_logo } from '../assets';
 
 const LoginPage = () => {
   const {
@@ -37,9 +38,15 @@ const LoginPage = () => {
         <div className='login-options'>
           <p>Or continue with</p>
           <div className='login-buttons'>
-            <button className='google-login'>Google</button>
-            <button className='facebook-login'>Meta</button>
-            <button className='apple-login'>Apple</button>
+            <button className='google-login'>
+              <img className='logo-img' src={google_logo} alt='google-logo' />
+            </button>
+            <button className='facebook-login'>
+              <img className='logo-img' src={meta_logo} alt='meta-logo' />
+            </button>
+            <button className='apple-login'>
+              <img className='logo-img' src={apple_logo} alt='apple-logo' />
+            </button>
           </div>
         </div>
       </div>
---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import apple_logo from "../assets/apple_logo.png"
+import google_logo from "../assets/google_logo.png"
+import meta_logo from "../assets/meta_logo.png"
+
+export { apple_logo, google_logo, meta_logo }
